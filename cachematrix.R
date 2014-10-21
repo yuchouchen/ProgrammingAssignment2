252d291d6f7e854bfdf59e4b3514651f71877654
YUCHOU CHEN <yuchouchen@gmail.com>
2014-10-21 21:23:52 +0800

    upload cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+#
+## makeCacheMatrix: This function creates a special "matrix" object that can 
+##                  cache its inverse.
+#
+## cacheSolve: This function computes the inverse of the special "matrix" 
+##             returned by makeCacheMatrix above. If the inverse has already 
+##             been calculated (and the matrix has not changed), then the 
+##             cachesolve should retrieve the inverse from the cache.
+#
+## Take the makeVector & cachemean codes of the assignment as a key reference
+## Created by: YuChou Chen (Joe Chen)
+##
+## =============================================================================
 ## Write a short comment describing this function
+#
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      Im <- NULL   # Im means Inverse of matrix
+      set <- function(y) {
+            x <<- y
+            Im <<- NULL
+      }
+      get <- function() x
+      setInverse <- function(solve) Im <<- solve   # change mean to solve
+      getInverse <- function() Im
+      list(set = set, 
+           get = get,
+           setInverse = setInverse,
+           getInverse = getInverse)
 }
 
-
+## =============================================================================
 ## Write a short comment describing this function
+#
+## cacheSolve computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      Im <- x$getInverse() # Im means Inverse of matrix
+      if(!is.null(Im)) {
+            message("getting cached data")
+            return(Im)
+      }
+      data <- x$get()
+      Im <- solve(data, ...)
+      x$setInverse(Im)
+      Im
 }
